10ef16dc9a868feee5c2bf3e7b09ec327a52c0c0
willconstable <will_constable@msn.com>
2020-04-27 12:37:00 +0100

    add prototype layout

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const app = express();
 
+//Set template engine
+app.set("view engine", "ejs");
+//Set static file location
+app.use(express.static("public"));
+
+//ROUTES
+//INDEX
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+//REGISTER
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+//LOGIN
+app.get("/login", (req, res) => {
+    res.render("login");
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server started")
